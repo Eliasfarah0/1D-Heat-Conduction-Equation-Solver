bdcf6c8004312b1c1e408cac38dad15aeb15e232
Elias Farah <eliasfarah31@gmail.com>
2021-07-19 12:29:08 +0100

    Update analysis.h

--- Source Code/OOP/analysis.h
+++ Source Code/OOP/analysis.h
@@ -25,7 +25,7 @@
 class Analysis {
 	// Atributes
 	private:
-		double D_value, deltax, deltat, thickness, outputTime, t_surf, t_init; // respectively: diffusion coefficient, space step, time step, time which ,temperature of the sides, initial temperature. 
+		double D_value, deltax, deltat, thickness, outputTime, t_surf, t_init; // respectively: diffusion coefficient, space step, time step, time which ,temperature of the sides, initial temperature 
 		int DufortFirstStepMethod; // this integer will define witch method to use for getting the solution at the first time step of the Dufort-Frankel scheme
 	
 	public:
@@ -38,31 +38,34 @@ class Analysis {
 		void setDeltat(double t);
 		void setThickness(double L);
 		void setOutputTime(double outTime);
-		void setT_surf(double Tsurf); // void setTsurf(int Tsurf);
-		void setT_init(double Tinit); // void setTinit(int Tinit);
+		void setT_surf(double Tsurf);
+		void setT_init(double Tinit);
 		void setDufortFirstStepMethod(int choice);
 		
 		// Methods
 		Implicit initialiseImplicit(Implicit impl); // initialise the implicit object, especially define discret time and space domain
 		Explicit initialiseExplicit(Explicit expl); // initialise the Explicit object, especially define discret time and space domain
 		
-		// write in a .csv file, the numerical solution at each node, using the Dufort-Frankel scheme, for a CONSTANT t chosen.
+		// write in a .csv file, the numerical solution at each node, using the Dufort-Frankel scheme, for a CONSTANT t chosen
 		void printExplicit_duFordFrankel();
 		
-		// write in a .csv file, the numerical solution at each node, using the Richardson scheme, for a CONSTANT t chosen.		
+		// write in a .csv file, the numerical solution at each node, using the Richardson scheme, for a CONSTANT t chosen		
 		void printExplicit_richardson();
 		
-		// write in a .csv file, the numerical solution at each node, using the Laasonen simple implicit scheme, for a CONSTANT t chosen.
+		// write in a .csv file, the numerical solution at each node, using the Laasonen simple implicit scheme, for a CONSTANT t chosen
 		void printImplicit_laasonen();
 		
-		// write in a .csv file, the numerical solution at each node, using the CrankNicolson scheme, for a CONSTANT t chosen.
+		// write in a .csv file, the numerical solution at each node, using the CrankNicolson scheme, for a CONSTANT t chosen
 		void printImplicit_crankNicolson();
 		
-		// analytic solution for each nodes at the time we're looking for, in order to compare it with numerical solutions(errors)		
+		// analytic solution for each nodes at the time we're looking for, in order to compare it with numerical solutions(errors)	
 		Vector exact_solution();
 		void print_exact_solution();
 		
-		// show the errrors i.e. absolute difference betwteen numerical values and analytic values at each nodes
-		Vector printErrors(int scheme);
+		// show the errors i.e. absolute difference betwteen numerical values and analytic values at each nodes
+		Vector printErrors(int numerical_scheme);
+	
+		// write in a .csv file, the numerical solution at each time step until the duration chosen is reached, using a chosen numerical scheme, for a CONSTANT x
+		void printTimeFunction(double positionToSee, double timeToSee, int numerical_scheme);
 };
 #endif
